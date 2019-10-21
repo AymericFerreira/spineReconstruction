1f8fec7b98af4c1c6fc3ae2e55f4118e9145a081
aymeric <aymeric.ferreira@gmail.com>
2019-10-21 17:17:27 -0400

    some improvements for paths in imageEnhancement and marching_cube_reconstruction
    Add "deconvolvution" algorithm based on mean filter

---
+++ deconvolve.py
@@ -0,0 +1,19 @@
+import numpy as np
+import skimage.io
+
+
+def remove_mean(img, factor=0.2, loop=10):
+    for loopCounter in range(loop):
+        img = img - factor * np.mean(img)
+        img[img < 0] = 0
+    return img
+
+
+def remove_mean_and_save(img, factor=0.2, loop=10, filename=''):
+    if filename is None:
+        filename = f'Deconvolved_{loop}_img'
+    for loopCounter in range(loop):
+        img = img - factor * np.mean(img)
+        img[img < 0] = 0
+    skimage.io.imsave(filename, img.astype(np.uint16))
+
