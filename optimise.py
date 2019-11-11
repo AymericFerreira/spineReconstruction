759d5308c9c352a375960e02dbf3073772deada9
aymeric <aymeric.ferreira@gmail.com>
2019-11-11 17:48:11 -0500

    Add some documentations add and correct function to remove noise/small meshes

--- optimise.py
+++ optimise.py
@@ -48,22 +48,28 @@ def remove_small_meshes(mesh):
 
 def new_remove_small_meshes(mesh, tolerance=5):
     meshT = pymesh_to_trimesh(mesh)
-    meshList = meshT.split()
+    meshT.split()
     biggestMeshesList = []
-    for subMesh in meshList:
-        if len(subMesh.vertices) > tolerance /100 * len(meshT.vertices):
-            biggestMeshesList.append(subMesh)
-    if biggestMeshesList is not None:
-        if len(biggestMeshesList) > 1:
-            finalMesh = trimesh.util.concatenate(biggestMeshesList)
+    if meshT is list:
+        for subMesh in meshT:
+            if len(subMesh.vertices) > tolerance /100 * len(meshT.vertices):
+                print(len(subMesh.vertices), len(meshT.vertices))
+                biggestMeshesList.append(subMesh)
+        if biggestMeshesList is not []:
+            print(biggestMeshesList)
+            for bigSubMesh in biggestMeshesList:
+                print(len(bigSubMesh.vertices))
+            if len(biggestMeshesList) > 1:
+                finalMesh = trimesh.util.concatenate(biggestMeshesList)
+            else:
+                finalMesh = biggestMeshesList
+            return trimesh_to_pymesh(finalMesh)
+
         else:
-            finalMesh = biggestMeshesList
-        return trimesh_to_pymesh(finalMesh)
-
+            print('Mesh seems broken')
+            return mesh
     else:
-        print('Mesh seems broken')
         return mesh
-
 
 def remove_noise(mesh, tolerance=5):
     meshT = pymesh_to_trimesh(mesh)
@@ -141,9 +147,27 @@ def count_number_of_meshes(mesh):
 
 def fix_meshes(mesh, detail="normal"):
     """
-    A pipeline to optimise and fix mesh. # todo : More information about each steps
+    A pipeline to optimise and fix mesh based on pymesh Mesh object. # todo : More information about each steps
+
+    1. A box is created around the mesh.
+    2. A target length is found based on diagonal of the mesh box.
+    3. You can choose between 3 level of details, normal details settings seems to be a good compromise between
+    final mesh size and sufficient number of vertices. It highly depends on your final goal.
+    4. Remove degenerated triangles aka collinear triangles composed of 3 aligned points. The number of iterations is 5
+    and should remove all degenerated triangles
+    5. Remove isolated vertices, not connected to any faces or edges
+    6. Remove self intersection edges and faces which is not realistic
+    7. Remove duplicated faces
+    8. The removing of duplicated faces can leave some vertices alone, we will removed them
+    9. The calculation of outer hull volume is useful to be sure that the mesh is still ok
+    10. Remove obtuse triangles > 179 who is not realistic and increase computation time
+    11. We will remove potential duplicated faces again
+    12. And duplicated vertices again
+    13. Finally we will look if the mesh is broken or not. If yes we will try lower settings, if the lowest settings
+    broke the mesh we will return the initial mesh. If not, we will return the optimised mesh.
+
     :param mesh: Pymesh Mesh object to optimise
-    :param detail: string 'high', 'normal' or 'low' ('normal' as default)
+    :param detail: string 'high', 'normal' or 'low' ('normal' as default), or float/int
     Settings to choose the targeting minimum length of edges
     :return: Pymesh Mesh object
     An optimised mesh or not depending on detail settings and mesh quality
@@ -159,6 +183,11 @@ def fix_meshes(mesh, detail="normal"):
         target_len = diag_len * 2.5e-3
     elif detail == "low":
         target_len = diag_len * 1e-2
+    elif detail is float or detail is int and detail > 0:
+        target_len = diag_len * detail
+    else:
+        print('Details settings is invalid, must be "low", "normal", "high" or positive int or float')
+        quit()
 
     count = 0
     mesh, __ = pymesh.remove_degenerated_triangles(mesh, 5)
@@ -182,8 +211,8 @@ def fix_meshes(mesh, detail="normal"):
     mesh, __ = pymesh.remove_duplicated_faces(mesh)
     mesh, __ = pymesh.remove_duplicated_vertices(mesh)
     mesh = pymesh.compute_outer_hull(mesh)
-    mesh, __ = pymesh.remove_duplicated_faces(mesh)
     mesh, __ = pymesh.remove_obtuse_triangles(mesh, 179.0, 5)
+    mesh, __ = pymesh.remove_duplicated_faces(mesh)
     mesh, __ = pymesh.remove_isolated_vertices(mesh)
 
     if is_mesh_broken(mesh, meshCopy) is True:
