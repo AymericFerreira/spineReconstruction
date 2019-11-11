4fdebd32adf147e5c18e0023dee21e05522b05d0
aymeric <aymeric.ferreira@gmail.com>
2019-11-11 17:09:42 -0500

    Change file to start assisted segmentation for all images in Images folder Then the automatic reconstruction will begin with all images in segmentedImages folder
    Output mesh should be in optimisedMeshes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from optimise import *
 from chan_vese_segmentation import *
 from marching_cube_reconstruction import *
 
 
 if __name__ == "__main__":
-    for (dirpath, _, filenames) in os.walk("images/"):
+    for (dirpath, _, filenames) in os.walk("Images/"):
         for filename in filenames:
-            # filename_plan_segmentation(dirpath, filename)
+            filename_plan_segmentation(dirpath, filename)
+    for (dirpath, _, filenames) in os.walk("segmentedImages/"):
+        for filename in filenames:
             automatic_marching_cube_reconstruction(dirpath, filename)
