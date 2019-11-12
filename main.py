c4e12354e7b798a7c3fe2a08cd4049477c31656a
aymeric <aymeric.ferreira@gmail.com>
2019-11-12 09:24:02 -0500

    Add some comments to explain what the main sequences do

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from chan_vese_segmentation import *
-from marching_cube_reconstruction import *
+import chan_vese_segmentation
+import marching_cube_reconstruction
+import os
 
 
 if __name__ == "__main__":
+    # Segmentation of all images in Images/ folder.
     for (dirpath, _, filenames) in os.walk("Images/"):
         for filename in filenames:
-            filename_plan_segmentation(dirpath, filename)
+            chan_vese_segmentation.filename_plan_segmentation(dirpath, filename)
+    # Reconstruction  via marching cubes algorithm of all segmented images in segmentedImages/ folder.
     for (dirpath, _, filenames) in os.walk("segmentedImages/"):
         for filename in filenames:
-            automatic_marching_cube_reconstruction(dirpath, filename)
+            marching_cube_reconstruction.automatic_marching_cube_reconstruction(dirpath, filename)
